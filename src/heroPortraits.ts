a7ae19b2346eb4dd7de5975bc5ed659827e01737
BloopRockets <blooprocket@gmail.com>
2026-04-02 07:02:07 -0400

    Refactor code structure for improved readability and maintainability

--- src/heroPortraits.ts
+++ src/heroPortraits.ts
@@ -2,6 +2,7 @@ import { ImageSourcePropType } from 'react-native';
 
 const HERO_PORTRAITS: Record<string, ImageSourcePropType> = {
   h4: require('../IMG/HeroIcon/ThornBloodhide.png'),
+  h18: require('../IMG/HeroIcon/VarricDoomhowl.png'),
   h25: require('../IMG/HeroIcon/MaeveStormpalm.png'),
   h32: require('../IMG/HeroIcon/ValkyraShieldborn.png'),
   h35: require('../IMG/HeroIcon/VesperSilverbow.png'),
@@ -10,7 +11,10 @@ const HERO_PORTRAITS: Record<string, ImageSourcePropType> = {
   h48: require('../IMG/HeroIcon/ArctusFrostking.png'),
   h50: require('../IMG/HeroIcon/OrionSoulshaper.png'),
   h55: require('../IMG/HeroIcon/ZephyrStarreacher.png'),
+  h56: require('../IMG/HeroIcon/NyxVoidChosen.png'),
+  h57: require('../IMG/HeroIcon/ArchaonTimeWeaver.png'),
   h58: require('../IMG/HeroIcon/PyritessEternalFlame.png'),
+  h60: require('../IMG/HeroIcon/VoidSovereign.png'),
   h64: require('../IMG/HeroIcon/CelestialArchitect.png'),
 };
 
